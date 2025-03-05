1f2e638df10898085fac69cf0017236787011f63
BystedUCN <10632179@ucn.dk>
2025-03-05 10:03:33 +0100

    opgaverne blev løst

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,15 +10,44 @@
 */
 
 // 1.1 Fang .mySkills med en egnet metode
+const mySkillsEl = document.querySelector(".mySkills");
+
 // 1.2 Anvend document.createElement() til at skabe en div, to span og et meter tag
+const newDiv = document.createElement("div");
+const newSpan1 = document.createElement("span");
+const newSpan2 = document.createElement("span");
+const newMeter = document.createElement("meter"); 
+
+
 // 1.3 Anvend document.textContent til at indsætte data i 2*span(se ovenfor for indhold)
+newSpan1.textContent = "JavaScript"
+newSpan1.textContent = "3%";
+
 // 1.4 Anvend .setAttribute() til at sætte de tre attributter i meter-tagget (min, max og value)
+newMeter.setAttribute("min", "0");
+newMeter.setAttribute("max", "100");
+newMeter.setAttribute("value", "3");
+
 // 1.5 Anvend .append() til at indsætte <div> i .mySkills
+mySkillsEl.append(newDiv);
+
 // 1.6 Anvend .append() til at indsætte 2*span og 1*meter i <div>
+newDiv.append(newSpan1, newSpan2, newMeter);
 
 // 2. Anvend .cloneNode() til at klone den <li> med "Mit portfolio" og indsætte den to gange nedenfor (Ja, så kommer der til at stå "Mit portfolio" tre gange - pyt med det:) )
+const liEl = document.querySelector("li");
+
+for (i = 0; i < 3; i++) {
+    let newLi = liEl.cloneNode(true);
+    document.querySelector("ul").appendChild(newLi);
+}
 
 // 3. I footeren er der en div med class="oval". Anvend .style-objektet til at give den følgende:
 // width: 40px, height: 40px, background-color: pink, border-radius: 50%
+document.querySelector(".oval").style.width = "40px";
+document.querySelector(".oval").style.height = "40px";
+document.querySelector(".oval").style.backgroundColor = "pink";
+document.querySelector(".oval").style.borderRadius = "50%";
 
 // 4. Se i dit .css dokument nederst at der er en class kaldet .niceStyling. Anvend .classList.add() til at sætte denne klasse på .oval2
+document.querySelector(".oval2").classList.add("niceStyling");
